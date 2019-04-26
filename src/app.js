d349049a7654c54916768768909ef4d65c11da22
Jackmc <jackmc1122@gmail.com>
2019-04-26 11:21:50 -0700

    Wire up submit button.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById('submit');
+const userGuess = document.getElementById('guess');
+
+submitButton.addEventListener('click', () => {
+  console.log(submitButton, userGuess);
+});
+
