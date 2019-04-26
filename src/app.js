8c8710c89b7631c8dad891d88ec9b827c4be36a1
Jackmc <jackmc1122@gmail.com>
2019-04-26 14:37:45 -0700

    Add blanks for guessing characters.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import { checkGuess, words } from './check-guess.js';
 
 const submitButton = document.getElementById('submit');
 const userGuess = document.getElementById('guess');
+const blanks = document.getElementById('blanks');
 
 let random = Math.floor(Math.random() * words.length);
 let randomWord = words[random];
 console.log(randomWord);
 let emptyWordArray = Array(randomWord.length);
 let answerWordArray = randomWord.split('');
+
+
+for(let i = 0; i < randomWord.length; i++) {
+  let blankDiv = document.createElement('div');
+  blankDiv.classList.add('blank');
+  blanks.appendChild(blankDiv);
+  console.log('add div');
+}
 
 submitButton.addEventListener('click', () => {
   const indices = checkGuess(randomWord, userGuess.value);
