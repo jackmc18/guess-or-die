1cd3140fddc3181166386f868ab24641e9a03666
Jackmc <jackmc1122@gmail.com>
2019-04-26 11:46:14 -0700

    Add test for letter which doesn't exist in the word.

--- test/tests.js
+++ test/tests.js
@@ -38,3 +38,17 @@ test('Guess a letter which exists twice in the word expect [1, 3] back', functio
   //Assert
   assert.deepEqual(result, expected);
 });
+
+
+test('Guess a letter which doesn`t exist in the word expect [] back', function(assert) {
+  //Arrange
+  // Set up your parameters and expectations
+  const word = 'javascript';
+  const guess = 'u';
+  const expected = [];
+  //Act 
+  // Call the function you're testing and set the result to a const
+  const result = guessChecker(word, guess);
+  //Assert
+  assert.deepEqual(result, expected);
+});
