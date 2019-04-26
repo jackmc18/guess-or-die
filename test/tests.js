4c42f2f10e504d004c57400bee4146f4f0b52209
Jackmc <jackmc1122@gmail.com>
2019-04-26 11:41:55 -0700

    Add test for 1 existing letter in the string.

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,27 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+// const words = ['javascript', 'language', 'scripting', 'interpreted', 'dynamic',
+//   'programming', 'array', 'integer', 'string', 'method', 'property', 'argument'];
+
+function guessChecker(word, letter) {
+  let indices = [];
+  for(let i = 0; i < word.length; i++) {
+    if(word[i] === letter) {
+      indices.push(i);
+    }
+  }
+  return indices;
+}
+
+test('Guess a letter which exists in the word expect [0] back', function(assert) {
   //Arrange
   // Set up your parameters and expectations
-
+  const word = 'javascript';
+  const guess = 'j';
+  const expected = [0];
   //Act 
   // Call the function you're testing and set the result to a const
-
+  const result = guessChecker(word, guess);
   //Assert
-  assert.equal(true, false);
+  assert.deepEqual(result, expected);
 });
